82ca88c374c75e488eba36019cc8e817d3419e14
fgrr12 <32877932+fgrr12@users.noreply.github.com>
2018-02-25 23:28:04 -0600

    Modulo 1, primer avance

---
+++ app/src/main/java/com/example/stefany/proyecto/Splash.java
@@ -0,0 +1,31 @@
+package com.example.stefany.proyecto;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.content.Intent;
+import android.os.Handler;
+
+public class Splash extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent intent = new Intent(Splash.this, MainActivity.class);
+                startActivity(intent);
+
+            }
+        }, 3000);
+
+    }
+}
